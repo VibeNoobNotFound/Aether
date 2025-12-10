a47d66580e9b351b41c85ced874391277f2af2ee
NoobNotFound <chanirurajapakse@gmail.com>
2025-12-11 02:24:53 +0530

    Add initial Aether macOS app project
    Set up the Aether macOS app with Xcode project files, basic SwiftUI app structure, and asset catalogs. Includes AetherApp.swift, ContentView.swift, and default asset files for app icon and accent color.

---
+++ Aether.MacOS/Aether/AetherApp.swift
@@ -0,0 +1,17 @@
+//
+//  AetherApp.swift
+//  Aether
+//
+//  Created by Chaniru Rajapakse on 2025-12-11.
+//
+
+import SwiftUI
+
+@main
+struct AetherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
